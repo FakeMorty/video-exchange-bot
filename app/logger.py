4a64dbca04ec7843a7b109f5dccfa562e15731b8
FakeMorty <mortyf47@gmail.com>
2026-03-30 20:56:54 +0300

    Log update (create)

---
+++ app/logger.py
@@ -0,0 +1,51 @@
+import json
+import logging
+import traceback
+from datetime import datetime
+
+
+def get_logger(name: str) -> logging.Logger:
+    return logging.getLogger(name)
+
+
+def _make_payload(message: str, **kwargs) -> str:
+    payload = {
+        "time": datetime.utcnow().isoformat() + "Z",
+        "message": message,
+    }
+    payload.update(kwargs)
+    return json.dumps(payload, ensure_ascii=False)
+
+
+def setup_logging(level: int = logging.INFO):
+    root = logging.getLogger()
+    root.setLevel(level)
+
+    while root.handlers:
+        root.handlers.pop()
+
+    handler = logging.StreamHandler()
+    handler.setLevel(level)
+    handler.setFormatter(logging.Formatter("%(message)s"))
+    root.addHandler(handler)
+
+
+def log_info(logger: logging.Logger, message: str, **kwargs):
+    logger.info(_make_payload(message, **kwargs))
+
+
+def log_warning(logger: logging.Logger, message: str, **kwargs):
+    logger.warning(_make_payload(message, **kwargs))
+
+
+def log_error(logger: logging.Logger, message: str, **kwargs):
+    logger.error(_make_payload(message, **kwargs))
+
+
+def log_exception(logger: logging.Logger, message: str, **kwargs):
+    kwargs["traceback"] = traceback.format_exc()
+    logger.error(_make_payload(message, **kwargs))
+
+
+def log_event(logger: logging.Logger, level: int, message: str, **kwargs):
+    logger.log(level, _make_payload(message, **kwargs))
